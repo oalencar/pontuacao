32c3a0129b0d384de08d4193fde353bfd10a260c
Oscar Alencar <oscar.alencar@gmail.com>
2018-12-31 00:17:02 -0300

    ajuste de página inicial de filtro

--- resources/views/admin/scores/report/index.blade.php
+++ resources/views/admin/scores/report/index.blade.php
@@ -4,32 +4,47 @@
 @section('content')
     <h3 class="page-title">@lang('quickadmin.score.report')</h3>
 
-
-    <div class="row">
-        <form method="POST" action="{{ route('admin.scores.reportByCompanyName') }}">
-            <div class="col-xs-10 col-md-6">
-                <div class="form-group">
-                    <select class="form-control" name="company">
-                        <option>Selecione </option>
-                        @foreach($companies as $company)
-                            <option value="{{ $company->id }}">{{ $company->nome }}</option>
-                        @endforeach
-                    </select>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <strong>Filtrar</strong>
+        </div>
+        <div class="panel-body">
+            <form method="POST" action="{{ route('admin.scores.reportByCompanyName') }}" id="formulario-filtro">
+                <div class="row">
+                    <div class="col-xs-10 col-md-6">
+                        <div class="form-group">
+                            <label for="select-company">Empresa *</label>
+                            <select class="form-control" name="company" id="select-company">
+                                <option value="">Selecione</option>
+                                @foreach($companies as $company)
+                                    <option value="{{ $company->id }}">{{ $company->nome }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <button type="button" class="btn btn-primary" id="btn-filter">Filtrar</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-xs-2 col-md-2">
-                <div class="form-group">
-                    <button type="submit" class="btn btn-primary">Filtrar</button>
-                </div>
-            </div>
 
-            {{ @csrf_field() }}
-        </form>
+                {{ @csrf_field() }}
+            </form>
+        </div>
     </div>
+
+
 
 @stop
 
 @section('javascript')
     <script>
+        $('#btn-filter').on('click', (e) => {
+            let option = $('#select-company').val();
+            if (!option || option === undefined) {
+                alert('Necessário selecionar o campo');
+                return;
+            }
+            $('#formulario-filtro').submit();
+        })
     </script>
 @endsection
